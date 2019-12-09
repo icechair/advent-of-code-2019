1af5b0578900cf2545b1127180703f96132f202d
Martin Gabel <martin.gabel@icechair.de>
2019-12-09 10:16:41 +0100

    memory preparation

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,20 @@
+
+fn create_memory(data: String) -> Vec<u32> {
+    data.split(",").map(|x| x.parse().unwrap()).collect()
+}
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_create_memory() {
+        let mem = create_memory("1,9,10,3,2,3,11,0,99,30,40,50".to_string());
+        assert_eq!(mem.len(), 12 as usize);
+        assert_eq!(mem[3], 3);
+    }
 }
