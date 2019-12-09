6a086176f60b05ccebb16b36c2fd53466e98a0b4
Martin Gabel <martin.gabel@icechair.de>
2019-12-09 22:45:40 +0100

    Part 2 complete

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,28 +1,59 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+
 use std::io::{self, BufRead};
-fn check_rules_part1(pw: String) -> bool {
+
+type Maybe = Option<Vec<u32>>;
+
+fn check_common(pw: String) -> Maybe {
     if pw.len() < 6 {
-        return false;
+        return None;
     }
+    let mut list: Vec<u32> = vec![0; 10];
     let mut last = 0;
-    let mut double = false;
     for c in pw.chars() {
         last = match c.to_digit(10) {
             Some(v) => {
                 if last > v {
-                    return false;
+                    return None;
                 }
-                if v == last {
-                    double = true;
-                }
+                list[v as usize] = list[v as usize] + 1;
                 v
             }
-            None => return false,
-        };
+            None => return None,
+        }
     }
-    return double;
+    debug!("part2: list: {:?}", list);
+    Some(list)
+}
+
+fn check_part1(maybe: &Maybe) -> bool {
+    match maybe {
+        Some(list) => {
+            for item in list {
+                if *item >= 2 {
+                    return true;
+                }
+            }
+            false
+        }
+        None => false,
+    }
+}
+
+fn check_part2(maybe: &Maybe) -> bool {
+    match maybe {
+        Some(list) => {
+            for item in list {
+                if *item == 2 {
+                    return true;
+                }
+            }
+            false
+        }
+        None => false,
+    }
 }
 
 fn main() {
@@ -36,14 +67,22 @@ fn main() {
         .expect("no new line")
         .expect("cant read line");
     let range: Vec<u32> = line.split("-").map(|x| x.parse().unwrap()).collect();
+    debug!("range: {:?}", range);
     let mut n_part1 = 0;
+    let mut n_part2 = 0;
     for pw in range[0]..range[1] {
-        n_part1 = match check_rules_part1(pw.to_string()) {
+        let maybe = check_common(pw.to_string());
+        n_part1 = match check_part1(&maybe) {
             true => n_part1 + 1,
             false => n_part1,
+        };
+        n_part2 = match check_part2(&maybe) {
+            true => n_part2 + 1,
+            false => n_part2,
         }
     }
     println!("{}", n_part1);
+    println!("{}", n_part2);
 }
 
 #[cfg(test)]
@@ -53,10 +92,14 @@ mod test {
     #[test]
     fn test_check_rules() {
         env_logger::init();
-        assert_eq!(check_rules_part1("111111".to_string()), true);
-        assert_eq!(check_rules_part1("223450".to_string()), false);
-        assert_eq!(check_rules_part1("123789".to_string()), false);
-        assert_eq!(check_rules_part1("138241".to_string()), false);
-        assert_eq!(check_rules_part1("674034".to_string()), false);
+        assert_eq!(check_part1(&check_common("111111".to_string())), true);
+        assert_eq!(check_part1(&check_common("223450".to_string())), false);
+        assert_eq!(check_part1(&check_common("123789".to_string())), false);
+        assert_eq!(check_part1(&check_common("138241".to_string())), false);
+        assert_eq!(check_part1(&check_common("674034".to_string())), false);
+
+        assert_eq!(check_part2(&check_common("112233".to_string())), true);
+        assert_eq!(check_part2(&check_common("123444".to_string())), false);
+        assert_eq!(check_part2(&check_common("111122".to_string())), true);
     }
 }
